b6609f3df66d46fcd2002fe86040e598b3393b5e
Ian Luna Quiroz <ianmlunaq@gmail.com>
2021-02-24 12:37:44 -0800

    Add files via upload

---
+++ initials.cpp
@@ -0,0 +1,13 @@
+// initials.cpp
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "IIIII  M   M  L       QQQQQ  \n";
+    cout << "  I    MM MM  L      Q     Q \n";
+    cout << "  I    M M M  L      Q   Q Q \n";
+    cout << "  I    M   M  L      Q    QQ \n";
+    cout << "IIIII  M   M  LLLLL   QQQQ Q \n";
+
+    return 0;
+}
